1884b8c527da512c3e23c2e5322df8c560dfd18a
skymodss <klesingerdavid@gmail.com>
2025-04-26 18:33:14 +0200

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -174,14 +174,12 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
 								postDatabseId={databaseId}
 								className="inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 text-primary-foreground h-10 px-4 bg-neutral-50 hover:bg-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-neutral-200 border border-input rounded-full"
 							/>
-                                                        <button
-							   onClick={() => {
-        							   if (ncmazVideoUrl?.videoUrl) {
-           								   window.open(ncmazVideoUrl.videoUrl, '_blank');
-        							     }
-    							    }}
-                                                            className="inline-flex items-center transition-colors hover:bg-neutral-50 dark:hover:bg-neutral-800 justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background hover:text-accent-foreground h-10 px-4 py-2 col-span-2 transition-colors duration-200 hover:bg-accent"
-                                                        >
+                                                       <a
+    								href={ncmazVideoUrl?.videoUrl || '#'}
+    								target="_blank"
+    								rel="noopener noreferrer"
+    								className="inline-flex items-center transition-colors hover:bg-neutral-50 dark:hover:bg-neutral-800 justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background hover:text-accent-foreground h-10 px-4 py-2 col-span-2 transition-colors duration-200 hover:bg-accent"
+							>
                                                             <span className="flex items-center gap-2">
                                                                 <svg
                                                                     xmlns="http://www.w3.org/2000/svg"
@@ -201,7 +199,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
                                                                 </svg>
                                                                 Download
                                                             </span>
-                                                        </button>
+                                                        </a>
                                                     </div>
                                                 </div>
                                             </div>
