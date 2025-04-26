132b42c40fe34df208bea0ea509207c89bf3a4e1
skymodss <klesingerdavid@gmail.com>
2025-04-27 00:25:41 +0200

    Update CoreFreeform.tsx

--- src/wp-blocks/CoreFreeform.tsx
+++ src/wp-blocks/CoreFreeform.tsx
@@ -21,10 +21,10 @@ const CoreFreeform = (props: any) => {
 export function wrapTablesInDiv(html: string): string {
 	// Regex để tìm các thẻ <table> không nằm trong thẻ <code> và không đã được bao bọc bởi div.table-wrapper
 	const tableRegex =
-		/(?<!<code[^>]*>)(?<!<div class="table-wrapper">[\s\S]*?)(<table\b[^>]*>[\s\S]*?<\/table>)(?![^<]*<\/code>)/gi
+		/(?<!<code[^>]*>)(?<!<div class="table-wrapper mt-0 mb-0">[\s\S]*?)(<table\b[^>]*>[\s\S]*?<\/table>)(?![^<]*<\/code>)/gi
 
 	// Thay thế mỗi thẻ <table> phù hợp bằng một <div> bao quanh nó
-	return html.replace(tableRegex, '<div class="table-wrapper">$1</div>')
+	return html.replace(tableRegex, '<div class="table-wrapper mt-0 mb-0">$1</div>')
 }
 
 CoreFreeform.displayName = 'CoreFreeform'
