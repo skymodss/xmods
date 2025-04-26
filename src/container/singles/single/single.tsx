faa80245cdb589b6e2084d81c984bd6e09b49f9d
skymodss <klesingerdavid@gmail.com>
2025-04-26 23:38:50 +0200

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -236,9 +236,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
 							id="single-entry-content"
 							className="prose mx-auto max-w-screen-md lg:prose-lg dark:prose-invert ml-[0px] mr-[0px] mt-[0px] mb-[0px]"
 							ref={contentRef}
-						>
-							<div className="ml-[0px] mr-[0px] mt-[0px] mb-[0px]" blocks={blocks} />
-						</div>
+						></div>
 					</div>
                                     </div>
                                 </section>
