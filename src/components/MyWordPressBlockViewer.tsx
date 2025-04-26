eedd6a8f622340d7be7069a251ffe861694a1765
skymodss <klesingerdavid@gmail.com>
2025-04-27 00:47:06 +0200

    Update MyWordPressBlockViewer.tsx

--- src/components/MyWordPressBlockViewer.tsx
+++ src/components/MyWordPressBlockViewer.tsx
@@ -7,7 +7,7 @@ import { FC } from 'react'
 interface Props extends WordpressBlocksViewerProps {}
 
 const MyWordPressBlockViewer: FC<Props> = ({ blocks }) => {
-	return <WordPressBlocksViewer blocks={blocks} />
+	return <WordPressBlocksViewer className="ml-[0px] mr-[0px] mt-[0px] mb-[0px]" blocks={blocks} />
 }
 
 export default MyWordPressBlockViewer
