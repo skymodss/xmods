97729f7c8d3d78b0492972e341aa386b9e021759
skymodss <klesingerdavid@gmail.com>
2025-04-27 03:36:24 +0200

    Update CreateNewPostEditor.tsx

--- src/components/PostSubmissionEditor/CreateNewPostEditor.tsx
+++ src/components/PostSubmissionEditor/CreateNewPostEditor.tsx
@@ -55,6 +55,7 @@ interface Props {
 }
 
 const CreateNewPostEditor: FC<Props> = ({
+	showRightSidebar,
 	isEditingPostId,
 	isEditingPage,
 	isSubmittingPage,
@@ -447,7 +448,7 @@ const CreateNewPostEditor: FC<Props> = ({
 	const LOADING = loading || updatePostLoading
 	const ERROR = error || updatePostError
 
-	const renderPostTitle = () => {
+	const renderPostTitle = ({showRightSidebar}) => {
 		return (
 			<div className="w-full px-2.5 pb-10 pt-2.5 lg:py-10">
 				<div className="mx-auto w-full max-w-screen-md space-y-5 rounded-xl border text-card-foreground shadow-sm bg-card/70 backdrop-blur-sm pl-[24px] pr-[24px] pt-[24px] pb-[24px]">
@@ -502,6 +503,7 @@ const CreateNewPostEditor: FC<Props> = ({
 					<TiptapEditor
 						defaultContent={contentHTML}
 						onUpdate={debounceGetContentHtml}
+						className="editor nc-CommentCard__box flex flex-1 flex-col overflow-hidden rounded-xl border border-neutral-200  sm:text-base dark:border-neutral-700"
 					/>
 				</div>
 			</div>
