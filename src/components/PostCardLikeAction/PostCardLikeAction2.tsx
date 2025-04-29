1c1a8b7597ce4072cfb8484b61f27720644af613
skymodss <klesingerdavid@gmail.com>
2025-04-29 22:56:46 +0200

    Update PostCardLikeAction2.tsx

--- src/components/PostCardLikeAction/PostCardLikeAction2.tsx
+++ src/components/PostCardLikeAction/PostCardLikeAction2.tsx
@@ -252,7 +252,7 @@ const PostCardLikeAction2: FC<PostCardLikeActionProps> = ({
 			</div>
 
 			<span
-				className={`ml-[0px] min-w-[1.125rem] flex-shrink-0 text-start transition-colors duration-75 ${
+				className={`ml-[0px] flex-shrink-0 text-start transition-colors duration-75 ${
 					isLiked
 						? 'text-rose-600 dark:text-rose-500 ml-[0px]'
 						: 'text-neutral-900 dark:text-neutral-200 ml-[0px]'
