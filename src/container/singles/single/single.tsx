d8bffb525e37dd1999d63715b50476f373a97cf4
skymodss <klesingerdavid@gmail.com>
2025-04-29 22:29:17 +0200

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -305,7 +305,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
                                 </section>
                             </div>
                             <aside className="script-similar-scripts lg:col-span-1">
-                                <div className="rounded-lg border text-card-foreground shadow-sm sticky top-15 bg-card/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 lg:w-[400px] h-[auto] pb-[15px] lg:h-[860px]">
+                                <div className="rounded-xl border text-card-foreground shadow-sm sticky top-15 bg-card/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-center h-[auto] pb-[15px] lg:h-[860px]">
                                     <div className="flex flex-col space-y-1.5 p-6 pb-3">
                                       <SingleRelatedPosts2
                                         posts={relatedPosts}
