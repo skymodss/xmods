908bda1066cf08185bff4cab482dd4e729691b1b
skymodss <klesingerdavid@gmail.com>
2025-05-01 10:07:19 +0200

    Update SingleRelatedPosts2.tsx

--- src/container/singles/SingleRelatedPosts2.tsx
+++ src/container/singles/SingleRelatedPosts2.tsx
@@ -16,8 +16,7 @@ const SingleRelatedPosts2: FC<SingleRelatedPostsProps> = ({
   }
 
   return (
-    <div className="w-full space-y-4 border bg-card text-card-foreground shadow-sm bg-[rgb(255,255,255)] rounded-2xl pl-[14px] pr-[14px] pt-[20px] pb-[14px]">
-      <div class="text-2xl font-bold leading-none tracking-tight"><h2>Description</h2></div>
+    <div className="w-full space-y-4 border bg-card text-card-foreground shadow-sm bg-[rgb(255,255,255)] rounded-2xl pl-[14px] pr-[14px] pt-[14px] pb-[14px]">
       {posts.map((post, index) => (
         <div key={post.databaseId || index} className="mb-4 last:mb-0">
           <Card9 
