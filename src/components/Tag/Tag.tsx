abae708ed51e3ed47fa57bd3266667288d748b36
skymodss <klesingerdavid@gmail.com>
2025-05-01 11:22:45 +0200

    Update Tag.tsx

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -18,7 +18,7 @@ const Tag: FC<TagProps> = ({
 }) => {
 	return (
 		<Link
-			className={`nc-Tag inline-block rounded-lg bg-white px-3 py-2 text-sm text-neutral-600 transition-colors hover:bg-neutral-50 md:px-4 md:py-2.5 dark:bg-neutral-900 dark:text-neutral-300 dark:hover:bg-neutral-800 ${className}`}
+			className={`nc-Tag inline-block rounded-2xl bg-white px-3 py-2 text-sm text-neutral-600 transition-colors hover:bg-neutral-50 md:px-4 md:py-2.5 dark:bg-neutral-900 dark:text-neutral-300 dark:hover:bg-neutral-800 ${className}`}
 			href={uri}
 		>
 			{`${name}`}
