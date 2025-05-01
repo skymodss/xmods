ec221bbf2285de71940097427e6908a62f63123a
skymodss <klesingerdavid@gmail.com>
2025-05-01 09:53:17 +0200

    Update SingleRelatedPosts2.tsx

--- src/container/singles/SingleRelatedPosts2.tsx
+++ src/container/singles/SingleRelatedPosts2.tsx
@@ -16,7 +16,7 @@ const SingleRelatedPosts2: FC<SingleRelatedPostsProps> = ({
   }
 
   return (
-    <div className="w-full space-y-4 border bg-card text-card-foreground shadow-sm bg-[rgb(255,255,255)] rounded-2xl pl-[14px] pr-[14px] pt-[14px] pb-[14px]">ž
+    <div className="w-full space-y-4 border bg-card text-card-foreground shadow-sm bg-[rgb(255,255,255)] rounded-2xl pl-[14px] pr-[14px] pt-[14px] pb-[14px]">
       <h2 className="text-2xl font-bold leading-none tracking-tight">Similar Mods</h2> 
       {posts.map((post, index) => (
         <div key={post.databaseId || index} className="mb-4 last:mb-0">
