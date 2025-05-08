d2f610a44b7bd9141830f049141452c858225f99
skymodss <klesingerdavid@gmail.com>
2025-05-08 02:54:30 +0200

    Update getUserDataFromUserCardFragment.ts

--- src/utils/getUserDataFromUserCardFragment.ts
+++ src/utils/getUserDataFromUserCardFragment.ts
@@ -3,33 +3,45 @@ import {
   NC_IMAGE_MEDIA_FRAGMENT,
   NC_USER_FULL_FIELDS_FRAGMENT,
   NC_USER_SHORT_FOR_POST_CARD_FRAGMENT,
-  POST_CARD_FIELDS, // Dodajemo novi fragment
+  POST_CARD_FIELDS, // Dodajemo novi fragment za verified3
 } from "../fragments";
 import { NcmazFcImageFieldsFragment } from "@/__generated__/graphql";
 
-export function getUserDataFromPostCardFragment(
-  post:
-    | FragmentType<typeof POST_CARD_FIELDS> // Koristimo POST_CARD_FIELDS za podatke
+export function getUserDataFromUserCardFragment(
+  user:
+    | FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>
     | FragmentType<typeof NC_USER_SHORT_FOR_POST_CARD_FRAGMENT>
 ) {
-  // Koristimo POST_CARD_FIELDS za podatke autora, uključujući verified3
-  let postQuery = useFragment(
+  // Koristimo NC_USER_FULL_FIELDS_FRAGMENT za sve osnovne podatke
+  let query = useFragment(
+    NC_USER_FULL_FIELDS_FRAGMENT,
+    user as FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>
+  );
+
+  // Koristimo POST_CARD_FIELDS samo za verified3
+  let verified3Query = useFragment(
     POST_CARD_FIELDS,
-    post as FragmentType<typeof POST_CARD_FIELDS>
+    user as FragmentType<typeof POST_CARD_FIELDS>
   );
 
   let featuredImageMeta: NcmazFcImageFieldsFragment | undefined | null = null;
   featuredImageMeta = useFragment(
     NC_IMAGE_MEDIA_FRAGMENT,
-    postQuery.featuredImage?.node
+    query.ncUserMeta?.featuredImage?.node
+  );
+  let bgImageMeta: NcmazFcImageFieldsFragment | undefined | null = null;
+  bgImageMeta = useFragment(
+    NC_IMAGE_MEDIA_FRAGMENT,
+    query.ncUserMeta?.backgroundImage?.node
   );
 
   return {
-    ...postQuery.author.node, // Vraćamo podatke autora
-    verified3: postQuery.author.node.verified3 || false, // Dodajemo verified3
-    uri: postQuery.author.node.uri || "",
-    name: postQuery.author.node.name || "",
-    avatar: postQuery.author.node.avatar?.url || "",
+    ...query,
+    verified3: verified3Query.author?.node?.verified3 || false, // Isključivo koristimo POST_CARD_FIELDS za verified3
+    uri: query.uri || "",
+    name: query.name || "",
+    username: query.username || "",
     featuredImageMeta,
+    bgImageMeta,
   };
 }
