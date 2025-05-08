f1594844422dd8b306fef7081e7db428079710ba
skymodss <klesingerdavid@gmail.com>
2025-05-08 02:36:46 +0200

    Update CardAuthor2.tsx

--- src/components/CardAuthor2/CardAuthor2.tsx
+++ src/components/CardAuthor2/CardAuthor2.tsx
@@ -2,13 +2,11 @@ import { FC } from 'react'
 import Avatar from '@/components/Avatar/Avatar'
 import Link from 'next/link'
 import { NcmazFcUserFullFieldsFragment } from '@/__generated__/graphql'
-import { PostCardFieldsNotNcmazMedia4Fragment } from '@/__generated__/graphql'
 import { FragmentType } from '@/__generated__'
 import { NC_USER_FULL_FIELDS_FRAGMENT } from '@/fragments'
 import ncFormatDate from '@/utils/formatDate'
 import { getUserDataFromUserCardFragment } from '@/utils/getUserDataFromUserCardFragment'
 import getTrans from '@/utils/getTrans'
-
 
 const T = getTrans()
 
