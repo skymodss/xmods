688603f13d351b456cc586624b4404e7adf0cfec
skymodss <klesingerdavid@gmail.com>
2025-05-10 18:44:13 +0200

    Update PostCardMeta.tsx

--- src/components/PostCardMeta/PostCardMeta.tsx
+++ src/components/PostCardMeta/PostCardMeta.tsx
@@ -54,74 +54,75 @@ export function getVerified3FromUser(user: any): boolean | null {
   return user?.verified3 || null
 }
 
+
 export interface PostCardMetaProps {
-  className?: string
-  meta: {
-    date?: string
-    author?:
-      | FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>
-      | NcmazFcUserFullFieldsFragment
-  }
-  hiddenAvatar?: boolean
-  avatarSize?: string
+	className?: string
+	meta: {
+		date?: string
+		author?:
+			| FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>
+			| NcmazFcUserFullFieldsFragment
+	}
+	hiddenAvatar?: boolean
+	avatarSize?: string
 }
 
 const PostCardMeta: FC<PostCardMetaProps> = ({
-  className = 'leading-none text-xs',
-  meta,
-  hiddenAvatar = false,
-  avatarSize = 'h-7 w-7 text-sm',
+	className = 'leading-none text-xs',
+	meta,
+	hiddenAvatar = false,
+	avatarSize = 'h-7 w-7 text-sm',
 }) => {
-  const { date } = meta
+	const { date } = meta
 
-  const author = getUserDataFromUserCardFragment(
-    meta.author as FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>,
-  )
+	const author = getUserDataFromUserCardFragment(
+		meta.author as FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>,
+	)
 
-  // Dohvat verified3
-  const isVerified = getVerified3FromUser(author)
+	const isVerified = getVerified3FromUser(author)
 
-  if (!author?.databaseId && !date) {
-    return null
-  }
+	if (!author.databaseId && !date) {
+		return null
+	}
 
-  return (
-    <div
-      className={`nc-PostCardMeta inline-flex flex-wrap items-center text-neutral-800 dark:text-neutral-200 ${className}`}
-    >
-      {author?.databaseId && (
-        <Link
-          href={author?.uri || ''}
-          className="relative flex items-center space-x-2 rtl:space-x-reverse"
-        >
-          {!hiddenAvatar && (
-            <Avatar
-              radius="rounded-full"
-              sizeClass={avatarSize}
-              imgUrl={author.ncUserMeta?.featuredImage?.node?.sourceUrl || ''}
-              userName={author?.name || ''}
-            />
-          )}
-          <span className="block font-medium capitalize text-neutral-700 hover:text-black dark:text-neutral-300 dark:hover:text-white">
-            {author?.name || ''}{' '}
-            <span className="text-xs text-gray-500">
-              ({isVerified ? 'verificiran' : 'nije verificiran'})
-            </span>
-          </span>
-        </Link>
-      )}
-      <>
-        {author?.databaseId && (
-          <span className="mx-[6px] font-medium text-neutral-500 dark:text-neutral-400">
-            ·
-          </span>
-        )}
-        <span className="font-normal text-neutral-500 dark:text-neutral-400">
-          {ncFormatDate(date || '')}
-        </span>
-      </>
-    </div>
-  )
+	return (
+		<div
+			className={`nc-PostCardMeta inline-flex flex-wrap items-center text-neutral-800 dark:text-neutral-200 ${className}`}
+		>
+			
+			{author?.databaseId && (
+				<Link
+					href={author?.uri || ''}
+					className="relative flex items-center space-x-2 rtl:space-x-reverse"
+				>
+					{!hiddenAvatar && (
+						<Avatar
+							radius="rounded-full"
+							sizeClass={avatarSize}
+							imgUrl={author.featuredImageMeta?.sourceUrl || ''}
+							userName={author?.name || ''}
+						/>
+					)}
+					<span className="block font-medium capitalize text-neutral-700 hover:text-black dark:text-neutral-300 dark:hover:text-white">
+						{author?.name || ''}
+						<span className="text-xs text-gray-500">
+              				({isVerified ? 'verificiran' : 'nije verificiran'})
+            			</span>
+					</span>
+				</Link>
+			)}
+			<>
+				{author?.databaseId && (
+					<span className="mx-[6px] font-medium text-neutral-500 dark:text-neutral-400">
+						·
+					</span>
+				)}
+				<span className="font-normal text-neutral-500 dark:text-neutral-400">
+					{ncFormatDate(date || '')}
+				</span>
+			</>
+		</div>
+	)
 }
 
 export default PostCardMeta
