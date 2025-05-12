31cbb20de7eca2fda87ff18bc0371a682800f294
skymodss <klesingerdavid@gmail.com>
2025-05-12 02:13:22 +0200

    Update getUserDataFromUserCardFragment.ts

--- src/utils/getUserDataFromUserCardFragment.ts
+++ src/utils/getUserDataFromUserCardFragment.ts
@@ -31,7 +31,7 @@ export function getUserDataFromUserCardFragment(
     ...query,
     uri: query.uri || "",
     name: query.name || "",
-    twitterUrl: query?.ncUserMeta?.twitterUrl || "",
+    twitterUrl: query?.user?.ncUserMeta?.twitterUrl || "",
     username: query.username || "",
     featuredImageMeta,
     bgImageMeta,
