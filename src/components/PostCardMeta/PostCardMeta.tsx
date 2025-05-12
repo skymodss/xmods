58290a2ac799ab69d2fbfbabcb2a3992d74ea540
skymodss <klesingerdavid@gmail.com>
2025-05-12 23:26:31 +0200

    Update PostCardMeta.tsx

--- src/components/PostCardMeta/PostCardMeta.tsx
+++ src/components/PostCardMeta/PostCardMeta.tsx
@@ -6,7 +6,7 @@ import ncFormatDate from '@/utils/formatDate'
 import { FragmentType } from '@/__generated__'
 import { NC_USER_FULL_FIELDS_FRAGMENT } from '@/fragments'
 import { getUserDataFromUserCardFragment } from '@/utils/getUserDataFromUserCardFragment'
-import { getTwitterLinkStatusForUser } from '@/container/AuthorPageLayout'  
+import { getTwitterLinkStatus } from '@/container/AuthorPageLayout'  
 import VerifyIcon from '@/components/VerifyIcon'
 
 export interface PostCardMetaProps {
@@ -39,7 +39,7 @@ const PostCardMeta: FC<PostCardMetaProps> = ({
 	}
 
 	// Get Twitter link status
-	const twitterLinkStatus = getTwitterLinkStatusForUser(author?.ncUserMeta?.twitterUrl || '')
+	const twitterLinkStatus = getTwitterLinkStatus(author?.uri || '')
 
 	return (
 		<div
@@ -64,7 +64,7 @@ const PostCardMeta: FC<PostCardMetaProps> = ({
 					{twitterLinkStatus === 1 ? (
 						<VerifyIcon />
 					) : (
-						<p>sdsdsd</p>
+						<p></p>
 					)}
 				</Link>
 			)}
