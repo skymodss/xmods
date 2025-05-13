b38b0c0fa39bd67644ed428b006f073fb8ac3372
skymodss <klesingerdavid@gmail.com>
2025-05-14 00:41:30 +0200

    Update VerifyIcon2.tsx

--- src/components/VerifyIcon2.tsx
+++ src/components/VerifyIcon2.tsx
@@ -40,18 +40,18 @@ const VerifyIcon2: FC<VerifyIcon2Props> = ({
 				/>
 			</svg>
 			<div
-				className={`absolute bg-[rgb(255,255,255)] rounded-2xl border shadow-sm bg-card text-start text-neutral-900 transition-transform duration-200 dark:text-neutral-200 px-2 py-1 pl-[8px] pr-[8px] pt-[8px] pb-[8px] ${
+				className={`absolute bg-[rgb(255,255,255)] rounded-xl border shadow-sm bg-card text-start text-neutral-900 transition-transform duration-200 dark:text-neutral-200 px-2 py-1 pl-[8px] pr-[8px] pt-[8px] pb-[8px] ${
 					showTooltip ? 'scale-100 opacity-100' : 'scale-75 opacity-0'
 				}`}
 				style={{
-					top: '-30px', // Razmak iznad ikone
+					top: '-35px', // Razmak iznad ikone
 					left: '50%',
 					transform: 'translateX(-50%)',
 					opacity: showTooltip ? 1 : 0,
 					transition: 'transform 0.2s ease-in-out, opacity 0.2s ease-in-out',
 				}}
 			>
-				<h2 className="text-sm leading-none tracking-tight">{'Verified'}</h2>
+				<h2 className="text-sm font-medium leading-none tracking-tight">{'Verified'}</h2>
 			</div>
 		</span>
 	);
