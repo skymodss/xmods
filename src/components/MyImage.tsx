2189f469b1778645158d17423b6bb33768cfe122
skymodss <klesingerdavid@gmail.com>
2025-05-14 18:35:39 +0200

    Update MyImage.tsx

--- src/components/MyImage.tsx
+++ src/components/MyImage.tsx
@@ -14,8 +14,8 @@ const MyImage: FC<Props> = ({
 	return (
 		<Image
 			{...props}
-			className={`${props.className} ${
-				props.src ? '' : 'dark:brightness-75 dark:filter transition-transform duration-500 group-hover:scale-103'
+			className="transition-transform duration-500 group-hover:scale-103" {`${props.className} ${
+				props.src ? '' : 'dark:brightness-75 dark:filter'
 			}`}
 			src={props.src || '/images/placeholder.png'}
 		/>
