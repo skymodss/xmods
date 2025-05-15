be6cdb853c605bd2095738466530821c36b0bba5
skymodss <klesingerdavid@gmail.com>
2025-05-15 12:47:26 +0200

    Update PostFeaturedMedia.tsx

--- src/components/PostFeaturedMedia/PostFeaturedMedia.tsx
+++ src/components/PostFeaturedMedia/PostFeaturedMedia.tsx
@@ -94,7 +94,7 @@ const PostFeaturedMedia: FC<PostFeaturedMediaProps> = ({
 				<MyImage
 					alt={title || 'Post Featured Image'}
 					fill
-					className="object-cover duration-300 group-hover:scale-103"
+					className="object-cover duration-300 group-hover:scale-102"
 					src={featuredImage?.sourceUrl || ''}
 					sizes="(max-width: 600px) 480px, 800px"
 				/>
