4196b2447fc1e03fecc8ba3fd3a12038eee4bf8d
skymodss <klesingerdavid@gmail.com>
2025-05-16 01:21:59 +0200

    Update PostCardMetaV2.tsx

--- src/components/PostCardMeta/PostCardMetaV2.tsx
+++ src/components/PostCardMeta/PostCardMetaV2.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import Avatar from '@/components/Avatar/Avatar'
 import Link from 'next/link'
-import { NcmazFcUserFullFieldsFragment } from '@/__generated__/graphql'
+import { NcmazFcUserFullFieldsFragment, NcmazFcPostMetaFullFieldsFragment } from '@/__generated__/graphql'
 import { FragmentType } from '@/__generated__'
 import { NC_USER_FULL_FIELDS_FRAGMENT } from '@/fragments'
 import ncFormatDate from '@/utils/formatDate'
