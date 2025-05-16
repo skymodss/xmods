6526f435ed3a16f2b0cecb6a94e7c3a91b65a25f
skymodss <klesingerdavid@gmail.com>
2025-05-16 21:19:38 +0200

    Update PostCardMetaV2.tsx

--- src/components/PostCardMeta/PostCardMetaV2.tsx
+++ src/components/PostCardMeta/PostCardMetaV2.tsx
@@ -12,7 +12,7 @@ import convertNumbThousand from '@/utils/convertNumbThousand'
 
 export interface PostCardMetaV2Props {
 	meta: {
-		NcPostMetaData?: NcPostMetaData
+		ncPostMetaData?: ncPostMetaData
 		date?: string
 		author?:
 			| FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>
@@ -33,7 +33,7 @@ const PostCardMetaV2: FC<PostCardMetaV2Props> = ({
 	titleClassName = 'text-base',
 	avatarSize = 'h-9 w-9 text-base',
 }) => {
-	const { date, title, uri, NcPostMetaData } = meta
+	const { date, title, uri, ncPostMetaData } = meta
 
 	const author = getUserDataFromUserCardFragment(
 		meta.author as FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>,
