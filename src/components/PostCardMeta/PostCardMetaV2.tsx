2412053f13de804c7ba9a7d7f026617b2b5ab647
skymodss <klesingerdavid@gmail.com>
2025-05-16 21:23:42 +0200

    Update PostCardMetaV2.tsx

--- src/components/PostCardMeta/PostCardMetaV2.tsx
+++ src/components/PostCardMeta/PostCardMetaV2.tsx
@@ -12,7 +12,7 @@ import convertNumbThousand from '@/utils/convertNumbThousand'
 
 export interface PostCardMetaV2Props {
 	meta: {
-		ncPostMetaData?: NcPostMetaData
+		ncPostMetaData?: Maybe<NcPostMetaData>
 		date?: string
 		author?:
 			| FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>
