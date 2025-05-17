659c5e9c4b738e098c9d08a219c044526cdb4172
skymodss <klesingerdavid@gmail.com>
2025-05-17 20:15:17 +0200

    Update AuthorPageLayout.tsx

--- src/container/AuthorPageLayout.tsx
+++ src/container/AuthorPageLayout.tsx
@@ -293,7 +293,7 @@ const AuthorLayout: FC<Props> = ({ className = '', children, user }) => {
 									</a>
 								)}
 								<SocialsList socials={userSocials} />
-								<div className="absolute end-5 start-auto flex justify-end md:top-[50px] lg:top-[60px] gap-2 mt-[0px]">
+								<div className="absolute end-5 start-auto flex justify-end  md:top-[36px] lg:top-[66px] gap-2 mt-[36px]">
 									<SocialsShareDropdown sizeClass="w-10 h-10" />
 									<AccountActionDropdown
 										authorSlug={authorSlug}
