305af1a851d9c89fc76bcb94090ae57802030557
skymodss <klesingerdavid@gmail.com>
2025-05-30 23:33:10 +0200

    Update callback.tsx

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import jwt_decode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode'; // ISPRAVAN import
 
 export default function AuthCallback() {
   const router = useRouter();
@@ -9,7 +9,7 @@ export default function AuthCallback() {
     const { token } = router.query;
 
     if (token && typeof token === 'string') {
-      const user = jwt_decode(token);
+      const user = jwtDecode(token);
       localStorage.setItem('user', JSON.stringify(user));
       router.push('/');
     }
