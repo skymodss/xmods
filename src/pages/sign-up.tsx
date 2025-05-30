97056e8a2ac8ab2b6f79c9caf765f14956b655e4
skymodss <klesingerdavid@gmail.com>
2025-05-30 23:45:15 +0200

    Update sign-up.tsx

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -172,13 +172,7 @@ export default function SignUp() {
 					</div>
 
 					<div className="grid pt-2">
-						<a href="https://xdd-a1e468.ingress-comporellon.ewp.live/wp-login.php?loginSocial=google"
-   							data-plugin="nsl"
-   							data-action="connect"
-   							data-redirect="current"
-   							data-provider="google"
-   							data-popupwidth="600"
-  					 		data-popupheight="600">
+						<a href="https://xdd-a1e468.ingress-comporellon.ewp.live/wp-login.php?loginSocial=google" data-plugin="nsl" data-action="connect" data-redirect="current" data-provider="google" data-popupwidth="600" data-popupheight="600">
    							Click here to login or register
 						</a>
 	
