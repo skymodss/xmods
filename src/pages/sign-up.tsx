f344474511912a9863fce528f8c897edc7ba0310
skymodss <klesingerdavid@gmail.com>
2025-05-30 23:47:54 +0200

    Update sign-up.tsx

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -173,7 +173,7 @@ export default function SignUp() {
 
 					<div className="grid pt-2">
 						<a href="https://xdd-a1e468.ingress-comporellon.ewp.live/wp-login.php?loginSocial=google" data-plugin="nsl" data-action="connect" data-redirect="current" data-provider="google" data-popupwidth="600" data-popupheight="600">
-   							Click here to login or register
+   							Click here to login or register r
 						</a>
 	
 						<ButtonPrimary loading={loading}>{T['Sign up']}</ButtonPrimary>
