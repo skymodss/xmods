1bbd77622c89dc89bf8e6071668b12770f79a4b7
skymodss <klesingerdavid@gmail.com>
2025-06-01 09:34:21 +0200

    Create GoogleLoginButton.tsx

---
+++ src/components/GoogleLoginButton.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { signIn } from "next-auth/react";
+
+export default function GoogleLoginButton() {
+  return (
+    <button onClick={() => signIn("google")}>
+      Login with Google
+    </button>
+  );
+}
