cebc6901957e3badbd7d0e0baf41a6f531749e07
skymodss <klesingerdavid@gmail.com>
2025-06-01 09:37:11 +0200

    Create wpSocialLogin.ts

---
+++ src/utils/wpSocialLogin.ts
@@ -0,0 +1,9 @@
+export async function wpSocialLogin(email: string, name: string, google_id: string) {
+  const res = await fetch("https://tvoj-wp-sajt.com/wp-json/custom/v1/social-login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, name, google_id }),
+  });
+  if (!res.ok) throw new Error("WP social login failed");
+  return await res.json();
+}
