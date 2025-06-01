89bfd89f10c1e51b401ea6737f7444c3068ae0c0
skymodss <klesingerdavid@gmail.com>
2025-06-01 12:01:45 +0200

    Update wpSocialLogin.ts

--- src/utils/wpSocialLogin.ts
+++ src/utils/wpSocialLogin.ts
@@ -1,5 +1,5 @@
 export async function wpSocialLogin(email: string, name: string, google_id: string) {
-  const res = await fetch("https://xdd-a1e468.ingress-comporellon.ewp.live/wp-json/custom/v1/social-login", {
+  const res = await fetch("https://tvoj-wp-sajt.com/wp-json/custom/v1/social-login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, name, google_id }),
