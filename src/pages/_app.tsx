1d4e1a92f21297f8b854a02dbbc93233a04f29cd
skymodss <klesingerdavid@gmail.com>
2025-06-01 10:38:24 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,47 +15,6 @@ import themeJson from '@/../theme.json'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
 import { SessionProvider, useSession } from 'next-auth/react'
 
-// Dodaj proveru za klijent pre upotrebe useSession
-function WordpressAuthSync() {
-	if (typeof window === 'undefined') return null
-
-	const { data: session, status } = useSession()
-
-	useEffect(() => {
-		if (
-			status === 'authenticated' &&
-			session?.user?.email &&
-			session?.user?.name
-		) {
-			fetch(
-				'https://xdd-a1e468.ingress-comporellon.ewp.live/wp-json/custom/v1/social-login',
-				{
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify({
-						email: session.user.email,
-						name: session.user.name,
-						google_id: (session.user as any).sub || '',
-					}),
-				}
-			)
-				.then(res => res.json())
-				.then(data => {
-					if (data?.token) {
-						localStorage.setItem('wp_jwt', data.token)
-					}
-				})
-				.catch(err => {
-					console.error('WP sync error:', err)
-				})
-		}
-	}, [session, status])
-
-	return null
-}
-
 const poppins = Poppins({
 	subsets: ['latin'],
 	display: 'swap',
