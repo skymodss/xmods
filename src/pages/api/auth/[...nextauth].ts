9d30237a66b30bdbde23dffd693f9cc70d16cafb
skymodss <klesingerdavid@gmail.com>
2025-06-16 01:06:43 +0200

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,24 @@
+// pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
   ],
+  callbacks: {
+    async jwt({ token, profile }) {
+      if (profile?.sub) {
+        token.sub = profile.sub;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.sub = token.sub; // <- OVDE dodaješ Google ID u session.user
+      return session;
+    }
+  }
 });
