2d5a6904167f34a595d6b6b6a399fc42e1d9ad7a
skymodss <klesingerdavid@gmail.com>
2025-06-16 01:08:35 +0200

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,24 +1,32 @@
-// pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error("Google Client ID/Secret nisu definisani u env!");
+}
 
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
   ],
   callbacks: {
     async jwt({ token, profile }) {
+      // Dodaj Google ID (sub) u token
       if (profile?.sub) {
         token.sub = profile.sub;
       }
       return token;
     },
     async session({ session, token }) {
-      session.user.sub = token.sub; // <- OVDE dodaješ Google ID u session.user
+      // Dodaj Google ID (sub) u session.user
+      if (session.user && token.sub) {
+        // @ts-ignore
+        session.user.sub = token.sub;
+      }
       return session;
-    }
-  }
+    },
+  },
 });
