5b30e78ccaab3d1573adb5b4f091e859629c8a4a
skymodss <klesingerdavid@gmail.com>
2025-06-16 12:19:19 +0200

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,21 +1,29 @@
-callbacks: {
-  async jwt({ token, profile }) {
-    if (profile) {
-      token.sub = profile.sub;
-      token.email = profile.email;
-      token.name = profile.name;
-    }
-    return token;
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, profile }) {
+      if (profile) {
+        token.sub = profile.sub;
+        token.email = profile.email;
+        token.name = profile.name;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        (session.user as any).sub = token.sub;
+        (session.user as any).email = token.email;
+        (session.user as any).name = token.name;
+      }
+      return session;
+    },
   },
-  async session({ session, token }) {
-    if (session.user) {
-      // @ts-ignore
-      session.user.sub = token.sub;
-      // @ts-ignore
-      session.user.email = token.email;
-      // @ts-ignore
-      session.user.name = token.name;
-    }
-    return session;
-  },
-},
+});
