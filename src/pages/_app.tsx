5e45ab8d4a22545c77dfd54e7da61062362a8405
skymodss <klesingerdavid@gmail.com>
2025-06-20 21:30:05 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -45,6 +45,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 							}
 						`}</style>
 						<NextNProgress color="#818cf8" />
+						<Component {...pageProps} key={router.asPath} />
 						<SessionProvider session={pageProps.session}>
       							<WordpressAuthSync />
     						</SessionProvider>
