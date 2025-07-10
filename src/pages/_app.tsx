ebf0785dc9272cc52337be84411312d3d3565009
skymodss <klesingerdavid@gmail.com>
2025-07-10 22:19:19 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@/../faust.config'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { FaustProvider } from '@faustwp/core'
 import '@/styles/globals.css'
@@ -16,52 +16,86 @@ import { GoogleAnalytics } from 'nextjs-google-analytics'
 import dynamic from "next/dynamic"
 const WordpressAuthSync = dynamic(() => import("@/components/WordpressAuthSync"), { ssr: false })
 import { SessionProvider } from "next-auth/react";
-import { AuthProvider } from "../context/AuthContext";
+// import { AuthProvider } from "../context/AuthContext"; // Ostavljen za kasniju upotrebu ako treba
+
+// Helper funkcije za detekciju tipa login-a
+function isGoogleJwtUser() {
+  return typeof window !== "undefined" && !!localStorage.getItem('wp_jwt');
+}
+
+async function isClassicWpUser() {
+  try {
+    const res = await fetch('/wp-json/wp/v2/users/me', {
+      credentials: 'include'
+    });
+    return res.ok;
+  } catch {
+    return false;
+  }
+}
 
 const poppins = Poppins({
-	subsets: ['latin'],
-	display: 'swap',
-	weight: ['300', '400', '500', '600', '700', '800','900'],
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
 })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	const router = useRouter()
+  const router = useRouter();
+  const [authMode, setAuthMode] = useState<"google" | "classic" | "none">("none");
 
-	return (
-		<>
-			<GoogleAnalytics trackPageViews />
+  useEffect(() => {
+    // Detekcija tipa autentifikacije
+    if (isGoogleJwtUser()) {
+      setAuthMode("google");
+    } else {
+      isClassicWpUser().then((isLoggedIn) => {
+        if (isLoggedIn) {
+          setAuthMode("classic");
+        } else {
+          setAuthMode("none");
+        }
+      });
+    }
+  }, []);
 
-			<FaustProvider pageProps={pageProps}>
-				<WordPressBlocksProvider
-					config={{
-						blocks,
-						theme: fromThemeJson(themeJson),
-					}}
-				>
-					<SiteWrapperProvider {...pageProps}>
-						<style jsx global>{`
-							html {
-								font-family: ${poppins.style.fontFamily};
-							}
-						`}</style>
-						<NextNProgress color="#818cf8" />
-						<Component {...pageProps} key={router.asPath} />
-						<SessionProvider session={pageProps.session}>
-      							<WordpressAuthSync />
-    						</SessionProvider>
-						<Toaster
-							position="bottom-left"
-							toastOptions={{
-								style: {
-									fontSize: '14px',
-									borderRadius: '0.75rem',
-								},
-							}}
-							containerClassName="text-sm"
-						/>
-					</SiteWrapperProvider>
-				</WordPressBlocksProvider>
-			</FaustProvider>
-		</>
-	)
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+
+      <FaustProvider pageProps={pageProps}>
+        <WordPressBlocksProvider
+          config={{
+            blocks,
+            theme: fromThemeJson(themeJson),
+          }}
+        >
+          <SiteWrapperProvider {...pageProps}>
+            <style jsx global>{`
+              html {
+                font-family: ${poppins.style.fontFamily};
+              }
+            `}</style>
+            <NextNProgress color="#818cf8" />
+            {/* Proslijedi authMode kao prop ako trebaš razlikovati */}
+            <Component {...pageProps} key={router.asPath} authMode={authMode} />
+            {/* Google login (JWT) sync aktivan samo za Google korisnike */}
+            <SessionProvider session={pageProps.session}>
+              <WordpressAuthSync />
+            </SessionProvider>
+            <Toaster
+              position="bottom-left"
+              toastOptions={{
+                style: {
+                  fontSize: '14px',
+                  borderRadius: '0.75rem',
+                },
+              }}
+              containerClassName="text-sm"
+            />
+          </SiteWrapperProvider>
+        </WordPressBlocksProvider>
+      </FaustProvider>
+    </>
+  )
 }
