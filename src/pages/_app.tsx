2d1657175baf0735639aa1df0bf0566138aab252
skymodss <klesingerdavid@gmail.com>
2025-07-11 00:38:45 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,7 @@ const WordpressAuthSync = dynamic(() => import("@/components/WordpressAuthSync")
 import { SessionProvider } from "next-auth/react"
 import { AuthProvider } from "../context/AuthContext"
 import { ApolloProvider } from "@apollo/client";
-import client from "@/lib/apolloClient";
+import client from "@/utils/apolloClient";
 
 const poppins = Poppins({
   subsets: ['latin'],
