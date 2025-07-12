4e4495414ab642d8243523a8fd1c1249b8c66e3f
skymodss <klesingerdavid@gmail.com>
2025-07-12 07:20:42 +0200

    Create useMe.ts

---
+++ src/hooks/useMe.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { gql } from "graphql-request";
+import { useGqlClient } from "./useGqlClient";
+
+const GET_ME = gql`
+  query GetMe {
+    me {
+      id
+      name
+      email
+    }
+  }
+`;
+
+export function useMe() {
+  const client = useGqlClient();
+  const [user, setUser] = useState<{ id: string; name: string; email: string }>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    client.request(GET_ME).then((data) => {
+      setUser(data.me);
+      setLoading(false);
+    });
+  }, [client]);
+
+  return { user, loading };
+}
