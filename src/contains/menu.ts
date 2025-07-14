07bf563123ca5007363edd90a02cc5603fc3fc06
skymodss <klesingerdavid@gmail.com>
2025-07-14 10:49:54 +0200

    Update menu.ts

--- src/contains/menu.ts
+++ src/contains/menu.ts
@@ -2,3 +2,5 @@ import { MenuLocationEnum } from "@/__generated__/graphql";
 
 export const PRIMARY_LOCATION = MenuLocationEnum.Primary;
 export const FOOTER_LOCATION = MenuLocationEnum.Footer;
+
+//99776
