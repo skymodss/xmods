745af1aedddbdee733f20570c60a9bfe31f1f23d
skymodss <klesingerdavid@gmail.com>
2025-07-29 14:12:04 +0200

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -54,13 +54,13 @@ export default function Footer({ menuItems }: Props) {
 			<h2 id="footer-heading" className="sr-only">
 				Footer
 			</h2>
-			<div className="mx-auto max-w-7xl px-6 pb-8 pt-20 sm:pt-24 lg:px-8 lg:pt-28">
+			<div className="mx-auto max-w-7xl px-6 pb-8">
 				<div className="xl:grid xl:grid-cols-3 xl:gap-8">
 					<div className="grid grid-cols-2 gap-8 md:grid-cols-4 xl:col-span-2">
 						{menus.map(renderMenuItem)}
 					</div>
 				</div>
-				<div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24 dark:border-neutral-700">
+				<div className="mt-1 border-gray-900/10 pt-8 sm:mt-1 md:flex md:items-center md:justify-between lg:mt-1 dark:border-neutral-700">
 					<div className="flex flex-wrap gap-x-6 gap-y-3 md:order-2">
 						{NC_SITE_SETTINGS.site_socials?.map((item) => (
 							<a
