588a48aec56c8adfa444e55ac62e3f8430311252
skymodss <klesingerdavid@gmail.com>
2025-07-29 20:23:29 +0200

    Update SwitchDarkMode2.tsx

--- src/components/SwitchDarkMode/SwitchDarkMode2.tsx
+++ src/components/SwitchDarkMode/SwitchDarkMode2.tsx
@@ -2,40 +2,32 @@
 
 import { Switch } from '@headlessui/react'
 import { useThemeMode } from '@/hooks/useThemeMode'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 export interface SwitchDarkMode2Props {
-  className?: string
+	className?: string
 }
 const SwitchDarkMode2: React.FC<SwitchDarkMode2Props> = ({}) => {
-  const { _toogleDarkMode, isDarkMode, toDark } = useThemeMode()
+	const { _toogleDarkMode, isDarkMode, toDark, toLight } = useThemeMode()
 
-  // Postavi dark mode na početku
-  useEffect(() => {
-    if (!isDarkMode) {
-      toDark()
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  return (
-    <div className="inline-flex">
-      <span className="sr-only">Enable dark mode</span>
-      <Switch
-        checked={isDarkMode}
-        onChange={_toogleDarkMode}
-        className={`${isDarkMode ? 'bg-teal-600' : 'bg-gray-300'} relative inline-flex h-[22px] w-[42px] shrink-0 cursor-pointer rounded-full border-4 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
-      >
-        <span className="sr-only">Enable dark mode</span>
-        <span
-          aria-hidden="true"
-          className={`${
-            isDarkMode ? 'translate-x-5 rtl:-translate-x-5' : 'translate-x-0'
-          } pointer-events-none inline-block h-[14px] w-[14px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-        />
-      </Switch>
-    </div>
-  )
+	return (
+		<div className="inline-flex">
+			<span className="sr-only">Enable dark mode</span>
+			<Switch
+				checked={isDarkMode}
+				onChange={_toogleDarkMode}
+				className={`${isDarkMode ? 'bg-teal-600' : 'bg-gray-300'} relative inline-flex h-[22px] w-[42px] shrink-0 cursor-pointer rounded-full border-4 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+			>
+				<span className="sr-only">Enable dark mode</span>
+				<span
+					aria-hidden="true"
+					className={`${
+						isDarkMode ? 'translate-x-5 rtl:-translate-x-5' : 'translate-x-0'
+					} pointer-events-none inline-block h-[14px] w-[14px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+				/>
+			</Switch>
+		</div>
+	)
 }
 
 export default SwitchDarkMode2
