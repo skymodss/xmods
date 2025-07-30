f493f7d2693a0ae9c362fb3c8b4286a360fa8862
skymodss <klesingerdavid@gmail.com>
2025-07-30 21:29:36 +0200

    Update single.tsx

--- src/container/singles/single/single.tsx
+++ src/container/singles/single/single.tsx
@@ -207,7 +207,7 @@ const SingleType1: FC<SingleType1Props> = ({ post, showRightSidebar }) => {
     								rel="noopener noreferrer"
     								className="inline-flex dark:zeleni zabut bg-zelena items-center transition-colors hover:bg-neutral-50 dark:hover:bg-neutral-25 justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border-input bg-background hover:text-accent-foreground h-10 px-4 py-2 col-span-2 transition-colors duration-200 hover:bg-accent"
 							>
-                                                            <span className="flex items-center gap-2">
+                                                            <span className="flex items-center stroke-[3] font-bold gap-2">
                                                                 <svg
                                                                     xmlns="http://www.w3.org/2000/svg"
                                                                     width="24"
