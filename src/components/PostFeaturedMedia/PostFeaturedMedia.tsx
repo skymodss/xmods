e99802bb05f5ec88087cbed7589e08ba157b3b25
skymodss <klesingerdavid@gmail.com>
2025-08-01 23:52:41 +0200

    Update PostFeaturedMedia.tsx

--- src/components/PostFeaturedMedia/PostFeaturedMedia.tsx
+++ src/components/PostFeaturedMedia/PostFeaturedMedia.tsx
@@ -10,115 +10,116 @@ import { PostDataFragmentType } from '@/data/types'
 import { getPostDataFromPostFragment } from '@/utils/getPostDataFromPostFragment'
 import MyImage from '../MyImage'
 
-
 export interface PostFeaturedMediaProps {
-	className?: string
-	post: PostDataFragmentType
-	isHover?: boolean
+  className?: string
+  post: PostDataFragmentType
+  isHover?: boolean
 }
 
 const PostFeaturedMedia: FC<PostFeaturedMediaProps> = ({
-	className = 'w-full h-full',
-	post,
-	isHover = false,
+  className = 'w-full h-full',
+  post,
+  isHover = false,
 }) => {
-	const {
-		title,
-		date,
-		categories,
-		excerpt,
-		author,
-		postFormats: postType,
-		featuredImage,
-		ncPostMetaData,
-		commentCount,
-		uri,
-		databaseId,
-		ncmazGalleryImgs,
-		ncmazVideoUrl,
-		ncmazAudioUrl,
-	} = getPostDataFromPostFragment(post)
+  const {
+    title,
+    date,
+    categories,
+    excerpt,
+    author,
+    postFormats: postType,
+    featuredImage,
+    ncPostMetaData,
+    commentCount,
+    uri,
+    databaseId,
+    ncmazGalleryImgs,
+    ncmazVideoUrl,
+    ncmazAudioUrl,
+  } = getPostDataFromPostFragment(post)
 
-	const galleryImgs = [featuredImage, ...(ncmazGalleryImgs || [])]
-		.map(img => img?.sourceUrl)
-		.filter(img => !!img) as string[]
+  const galleryImgs = [featuredImage, ...(ncmazGalleryImgs || [])]
+    .map(img => img?.sourceUrl)
+    .filter(img => !!img) as string[]
 
-	const isPostMedia = () => postType === 'video' || postType === 'audio'
+  const isPostMedia = () => postType === 'video' || postType === 'audio'
 
-	const renderGallerySlider = () => {
-		if (!galleryImgs || !galleryImgs.length) {
-			return null
-		}
+  const renderGallerySlider = () => {
+    if (!galleryImgs || !galleryImgs.length) {
+      return null
+    }
 
-		return (
-			<GallerySlider
-				href={uri || ''}
-				galleryImgs={galleryImgs.filter(img => !!img) as string[]}
-				className="absolute inset-0 z-10"
-				galleryClass="absolute inset-0"
-				ratioClass="absolute inset-0"
-			/>
-		)
-	}
+    return (
+      <GallerySlider
+        href={uri || ''}
+        galleryImgs={galleryImgs.filter(img => !!img) as string[]}
+        className="absolute inset-0 z-10"
+        galleryClass="absolute inset-0"
+        ratioClass="absolute inset-0"
+      />
+    )
+  }
 
-	const renderContent = () => {
-		// GALLERY
-		if (postType === 'gallery') {
-			return renderGallerySlider()
-		}
+  const renderContent = () => {
+    // GALLERY
+    if (postType === 'gallery') {
+      return renderGallerySlider()
+    }
 
-		// VIDEO
-		if (postType === 'video' && !!ncmazVideoUrl?.videoUrl && isHover) {
-			return <MediaVideo isHover videoUrl={ncmazVideoUrl.videoUrl} />
-		}
+    // VIDEO
+    if (postType === 'video' && !!ncmazVideoUrl?.videoUrl && isHover) {
+      return <MediaVideo isHover videoUrl={ncmazVideoUrl.videoUrl} />
+    }
 
-		// AUDIO
-		if (postType === 'audio' && !!ncmazAudioUrl) {
-			return <MediaAudio post={post} />
-		}
+    // AUDIO
+    if (postType === 'audio' && !!ncmazAudioUrl) {
+      return <MediaAudio post={post} />
+    }
 
-		// ICON
-		return isPostMedia() ? (
-			<span className="absolute inset-0 flex items-center justify-center">
-				<PostTypeFeaturedIcon
-					className="transform cursor-pointer"
-					postType={postType}
-				/>
-			</span>
-		) : null
-	}
+    // ICON
+    return isPostMedia() ? (
+      <span className="absolute inset-0 flex items-center justify-center">
+        <PostTypeFeaturedIcon
+          className="transform cursor-pointer"
+          postType={postType}
+        />
+      </span>
+    ) : null
+  }
 
-	return (
-		<div className={`nc-PostFeaturedMedia relative ${className}`}>
-			{postType !== 'gallery' && (
-				<MyImage
-					alt=""
-					fill
-					className="h-full w-full object-cover rounded-3xl opacity-50 blur-3xl scale-100"
-					src={featuredImage?.sourceUrl || ''}
-					sizes="(max-width: 600px) 480px, 800px"
-				/>
-				<MyImage
-					alt={title || 'Post Featured Image'}
-					fill
-					className="object-cover duration-300 group-hover:scale-105"
-					src={featuredImage?.sourceUrl || ''}
-					sizes="(max-width: 600px) 480px, 800px"
-				/>
-			)}
-			{renderContent()}
-			{postType !== 'gallery' && (
-				<Link
-					href={uri || ''}
-					className={`absolute inset-0 block ${
-						!postType || postType === 'standard'
-							? 'opacity-0 transition-opacity group-hover:opacity-100'
-							: ''
-					}`}
-				/>
-			)}
-		</div>
-	)
+  return (
+    <div className={`nc-PostFeaturedMedia relative ${className}`}>
+      {postType !== 'gallery' && (
+        <>
+          <MyImage
+            alt=""
+            fill
+            className="h-full w-full object-cover rounded-3xl opacity-50 blur-3xl scale-100"
+            src={featuredImage?.sourceUrl || ''}
+            sizes="(max-width: 600px) 480px, 800px"
+          />
+          <MyImage
+            alt={title || 'Post Featured Image'}
+            fill
+            className="object-cover duration-300 group-hover:scale-105"
+            src={featuredImage?.sourceUrl || ''}
+            sizes="(max-width: 600px) 480px, 800px"
+          />
+        </>
+      )}
+      {renderContent()}
+      {postType !== 'gallery' && (
+        <Link
+          href={uri || ''}
+          className={`absolute inset-0 block ${
+            !postType || postType === 'standard'
+              ? 'opacity-0 transition-opacity group-hover:opacity-100'
+              : ''
+          }`}
+        />
+      )}
+    </div>
+  )
 }
 
 export default PostFeaturedMedia
