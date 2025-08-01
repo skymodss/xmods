cfad4aec32f1f0801abf77dcd4a74f3267ed7e7d
skymodss <klesingerdavid@gmail.com>
2025-08-01 20:13:46 +0200

    Update Textarea.tsx

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
 		return (
 			<textarea
 				ref={ref}
-				className={`block w-full rounded-xl border-neutral-200 bg-white text-sm hover:ring hover:ring-primary-200/50 focus:border-primary-300 focus:ring focus:ring-primary-200/50 dark:border-neutral-600 dark:bg-transparent dark:hover:ring-primary-500/30 dark:focus:ring-primary-500/30 ${className}`}
+				className={`block w-full rounded-xl border-2 border-neutral-200 bg-white text-sm hover:ring hover:ring-primary-200/50 focus:border-primary-300 focus:ring focus:ring-primary-200/50 dark:border-neutral-800 dark:bg-transparent dark:hover:ring-primary-500/30 dark:focus:ring-primary-500/30 ${className}`}
 				rows={4}
 				{...args}
 			>
