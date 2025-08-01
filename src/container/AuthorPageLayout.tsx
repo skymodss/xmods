2b1c36fa967b110528c1c359b3ac8e858c6f8434
skymodss <klesingerdavid@gmail.com>
2025-08-01 23:03:52 +0200

    Update AuthorPageLayout.tsx

--- src/container/AuthorPageLayout.tsx
+++ src/container/AuthorPageLayout.tsx
@@ -242,12 +242,12 @@ const AuthorLayout: FC<Props> = ({ className = '', children, user }) => {
 									getImageDataFromImageFragment(ncUserMeta?.backgroundImage?.node)
 										.sourceUrl
 								}
-								className="h-full w-full object-cover rounded-3xl shadow-ocak"
+								className="h-full w-full object-cover rounded-3xl shadow-2xl shadow-black/40"
 								fill
 								priority
 							/>
 						) : (
-							<div className="absolute inset-0 bg-neutral-200/70 rounded-3xl" />
+							<div className="absolute inset-0 bg-neutral-200/70 rounded-3xl shadow-2xl dark:bg-black/40" />
 						)}
 					</div>
 				</div>
