4d78c8adfff02c0432ee61f59ed5324328b0ada7
skymodss <klesingerdavid@gmail.com>
2025-08-01 23:21:41 +0200

    Update AuthorPageLayout.tsx

--- src/container/AuthorPageLayout.tsx
+++ src/container/AuthorPageLayout.tsx
@@ -58,7 +58,7 @@ const AuthorLayout: FC<Props> = ({ className = '', children, user }) => {
                   containerClassName="absolute inset-0"
                   sizes="(max-width: 1280px) 100vw, 1536px"
                   src={getImageDataFromImageFragment(ncUserMeta?.backgroundImage?.node).sourceUrl}
-                  className="h-full w-full object-cover rounded-3xl blur-xl opacity-50 scale-105"
+                  className="h-full w-full object-cover rounded-3xl blur-xl opacity-50 scale-100"
                   aria-hidden
                   fill
                   priority
@@ -75,7 +75,7 @@ const AuthorLayout: FC<Props> = ({ className = '', children, user }) => {
                 />
               </>
             ) : (
-              <div className="absolute inset-0 bg-neutral-200/70 rounded-3xl" />
+              <div className="absolute inset-0 bg-neutral-200/70 rounded-3xl shadow-2xl" />
             )}
           </div>
         </div>
